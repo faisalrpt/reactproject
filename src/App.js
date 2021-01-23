49ffed01894d9393502fe3357ccbebb1e204d0ac
Mohd_Faisal <mfaisalrpt@gmail.com>
2021-01-23 21:48:53 +0530

    upgrade project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import './App.css';
-import faisal_img from './img/faisal_img.JPG'
-function App() {
+import React from 'react';
+import {Switch,Route} from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+
+import Home from './components/Home';
+import About from './components/About';
+import Service from './components/Service';
+import Contact from './components/Contact';
+import Error from './components/Error';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import './App.css'
+
+const App = ()=>{
   return (
     <>
-    <div style={{textAlign: "center"}}>
-    <h1 style={{backgroundColor: "Blue",padding:"10px 0",color:"#fff"}}>My React App on Github</h1>
-    <h2>Author: Mohd Faisal</h2>
-    <img src={faisal_img} width="600px"/>
-    </div>
+    <Navbar />
+    <Switch>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/about" component={About} />
+    <Route exact path="/service" component={Service} />
+    <Route exact path="/contact" component={Contact} />
+    {/* <Route exact path="/Login" component={Login} /> */}
+    <Route component={Error} />
+    </Switch>
+    <Footer />
     </>
-  );
+  )
 }
-
 export default App;
